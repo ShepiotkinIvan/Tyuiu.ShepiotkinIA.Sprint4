c4e79595704efaf22fd5b0a81e5cfe59b2f07897
Tyuiu.ShepiotkinIA@outlook.com <Tyuiu.ShepiotkinIA@outlook.com>
2023-11-04 17:27:08 +0500

    Task3

---
+++ Tyuiu.ShepiotkinIA.Sprint4.Task3.V24.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.ShepiotkinIA.Sprint4.Task3.V24.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.ShepiotkinIA.Sprint4.Task3.V24.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f4f164bd-257e-44aa-a52e-86a752484f9e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
